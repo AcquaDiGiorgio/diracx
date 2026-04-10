eeb7c922bae3af1b346f3a2d30822210f3aab36c
AcquaDiGiorgio <64639359+AcquaDiGiorgio@users.noreply.github.com>
2026-04-10 15:44:04 +0200

    fix: typo

--- diracx-core/src/diracx/core/models/__init__.py
+++ diracx-core/src/diracx/core/models/__init__.py
@@ -35,7 +35,7 @@ __all__ = [
     "SetJobStatusReturn",
     # Replica Map
     "ReplicaMap",
-    # Sanbox
+    # Sandbox
     "ChecksumAlgorithm",
     "SandboxDownloadResponse",
     "SandboxFormat",
